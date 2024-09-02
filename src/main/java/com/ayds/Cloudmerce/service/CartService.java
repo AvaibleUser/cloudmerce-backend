de9478aa95fb897ae327ab49afe5e6cbe032f008
Elvis Aguilar <elvisaguilar201930304@cunoc.edu.gt>
2024-09-01 23:39:50 -0600

    feat(Cart): cambio

--- src/main/java/com/ayds/Cloudmerce/service/CartService.java
+++ src/main/java/com/ayds/Cloudmerce/service/CartService.java
@@ -6,17 +6,29 @@ import com.ayds.Cloudmerce.model.entity.OrderEntity;
 import com.ayds.Cloudmerce.model.entity.PaymentMethodEntity;
 import com.ayds.Cloudmerce.model.entity.ProcessStatusEntity;
 import com.ayds.Cloudmerce.repository.CartRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CartService {
+
+    @PersistenceContext
+    private EntityManager entityManager;
 
     private final CartRepository cartRepository;
     private final CartItemService cartItemService;
@@ -45,6 +57,11 @@ public class CartService {
         return this.orderService.updateStatusOrder(orderUpdate, processStatus);
     }
 
+    /**
+     * esta funcion es especificamente usada cuando se actulize la orden
+     * @param orderUpdate
+     * @param processStatus
+     */
     private void updateDateProcessOrder(OrderEntity orderUpdate, ProcessStatusEntity processStatus){
         LocalDateTime dateTime = LocalDateTime.now();
         switch (processStatus.getStatus()){
@@ -60,7 +77,6 @@ public class CartService {
                 break;
         }
     }
-
 
     @Transactional
     public CartDTO registerCart(CartDTO cartDTO) {
@@ -144,5 +160,91 @@ public class CartService {
             cartDTO.setItems(listDTO);
         }
         return cartDTO;
+    }
+
+    //apartado de busqueda por usuario + otros parametros
+
+    public List<CartEntity> getAllCartsUser(Integer userId){
+        return this.cartRepository.findAllByUserId(userId).orElse(List.of());
+    }
+
+    /**
+     *  funcion para obtener los carritos de un usuario en especifico, con parametros
+     * @param userId el id del usuario
+     * @param size para el maximo de devolucions
+     * @param startDate fecha del inicio para el filtro
+     * @param endDate fecha de final para el rango
+     * @param order asc o desc
+     * @param cartIdInit si se quiere iniciar en un carrito como punto de partida
+     * @return
+     */
+    public List<CartResponseDto> getCartsUserParams(Integer userId, int size, String startDate, String endDate, String order, int cartIdInit, int processStatus, int paymentMethod) {
+        // Crear CriteriaBuilder y CriteriaQuery
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<CartEntity> cq = cb.createQuery(CartEntity.class);
+        Root<CartEntity> cart = cq.from(CartEntity.class);
+
+        // Crear lista de predicados para aplicar filtros
+        List<Predicate> predicates = new ArrayList<>();
+        predicates.add(cb.equal(cart.get("userId"), userId));
+
+        this.addFilterDate(predicates,cart,startDate,endDate,cb);
+
+        // Aplicar filtro de cartIdInit
+        if (cartIdInit > 0) {
+            predicates.add(cb.greaterThanOrEqualTo(cart.get("id"), cartIdInit));
+        }
+
+        // aplica el filtro de paymethod y processstatus
+        this.addFilterPayMethod(paymentMethod,predicates,cart,cb);
+        this.addFilterProcessSatatus(processStatus,predicates,cart,cb);
+
+        // Aplicar los predicados a la consulta
+        cq.where(cb.and(predicates.toArray(new Predicate[0])));
+
+        this.addFilterOrder(cart,cq,cb,order);
+
+        // Ejecutar la consulta con el tamaño máximo de resultados
+        TypedQuery<CartEntity> query = entityManager.createQuery(cq);
+        query.setMaxResults(size);
+
+        return query.getResultList().stream()
+                .map(this::convertToCartDtoResponse)
+                .collect(Collectors.toList());
+    }
+
+    private void addFilterProcessSatatus(int processStatus, List<Predicate> predicates, Root<CartEntity> cart,  CriteriaBuilder cb){
+        if (processStatus > 0){
+            predicates.add(cb.equal(cart.get("statusId"), processStatus));
+        }
+    }
+
+    private void addFilterPayMethod(int paymentMethod, List<Predicate> predicates, Root<CartEntity> cart,  CriteriaBuilder cb){
+        if (paymentMethod > 0){
+            predicates.add(cb.equal(cart.get("paymentMethodId"), paymentMethod));
+        }
+    }
+
+    private void addFilterOrder(Root<CartEntity> cart, CriteriaQuery<CartEntity> cq, CriteriaBuilder cb, String order){
+        // Ordenar la consulta por fecha de creación
+        if ("desc".equalsIgnoreCase(order)) {
+            cq.orderBy(cb.desc(cart.get("createdAt")));
+        } else {
+            cq.orderBy(cb.asc(cart.get("createdAt")));
+        }
+    }
+
+    private void addFilterDate(List<Predicate> predicates, Root<CartEntity> cart , String startDate, String endDate, CriteriaBuilder cb ){
+        // Parsear y aplicar filtro de startDate
+        if (startDate != null && !startDate.isEmpty()) {
+            LocalDate start = LocalDate.parse(startDate);  // Usar LocalDate para manejar solo fechas
+            predicates.add(cb.greaterThanOrEqualTo(cart.get("createdAt").as(LocalDate.class), start));
+        }
+
+        // Parsear y aplicar filtro de endDate
+        if (endDate != null && !endDate.isEmpty()) {
+            LocalDate end = LocalDate.parse(endDate);
+            predicates.add(cb.lessThanOrEqualTo(cart.get("createdAt").as(LocalDate.class), end));
+        }
     }
 }
